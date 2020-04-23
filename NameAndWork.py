211404887895c46abda42223390363a32cf6d7db
Ilya <i.panasevich@students.clark.edu>
2020-04-22 19:05:11 -0700

    Input name and location to complete function.

---
+++ NameAndWork.py
@@ -0,0 +1,19 @@
+"""
+CTEC 121
+Ilya Panasevich
+Programming assignment 1 part 2
+create a main function and add print statements to it
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main(name, location):
+    print("Hello " + name + ", do you work at " + location + "?" )
+
+main("John", "Walmart")
+main("Jessica", "Target")
+
