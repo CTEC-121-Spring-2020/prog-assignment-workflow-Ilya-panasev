ac12747c6819b1311d20f2449d4a1062faf7d899
Ilya <i.panasevich@students.clark.edu>
2020-04-22 19:28:26 -0700

    Personalized Hello and 2 extra print statements

---
+++ HelloV2.py
@@ -0,0 +1,20 @@
+"""
+CTEC 121
+Ilya Panasevich
+Programming assignment 1
+Print your own version of hello, add at least 2 more print statements
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print("Hello fellow programmers")
+    print("VScode is being more cooperative now")
+    print("I think I'm getting the hang of this")
+    
+
+main()    
